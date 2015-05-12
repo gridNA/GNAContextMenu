33b6e8678ff26ade101664d3f913b91d0e454aa3
Kateryna Gridina <kgridina@zalando-07855.corp.ad.zalando.net>
2015-05-12 11:48:03 +0200

    Initial Commit

---
+++ myfeedPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myfeedPOC
+//
+//  Created by Kateryna Gridina on 12/05/15.
+//  Copyright (c) 2015 zalando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
