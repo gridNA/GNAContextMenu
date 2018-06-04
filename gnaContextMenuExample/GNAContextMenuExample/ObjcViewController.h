e34b9364d784ed11bc351ad352dc20ffdb1d24a8
Kateryna Gridina <kateryna.gridina@zalando.de>
2018-06-04 17:10:47 +0200

    add  Swift 4.1 support, objc example

---
+++ gnaContextMenuExample/GNAContextMenuExample/ObjcViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObjcViewController.h
+//  GNAContextMenuExample
+//
+//  Created by Kateryna Gridina on 04.06.18.
+//  Copyright © 2018 Kateryna Gridina. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ObjcViewController : NSObject
+
+@end
---
+++ gnaContextMenuExample/GNAContextMenuExample/ObjcViewController.m
@@ -0,0 +1,13 @@
+//
+//  ObjcViewController.m
+//  GNAContextMenuExample
+//
+//  Created by Kateryna Gridina on 04.06.18.
+//  Copyright © 2018 Kateryna Gridina. All rights reserved.
+//
+
+#import "ObjcViewController.h"
+
+@implementation ObjcViewController
+
+@end
